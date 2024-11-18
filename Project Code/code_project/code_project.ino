fcef89ff5501810291b52427d34ba2fd9295e735
Bernard Joseph <37205335+ikeben@users.noreply.github.com>
2024-11-18 23:17:10 +0100

    Update code_project.ino

--- Project Code/code_project/code_project.ino
+++ Project Code/code_project/code_project.ino
@@ -1,153 +1,164 @@
-/* 500Level project;
+/* 500Level Project
 BERNARD JOSEPH
 SAMUEL ELEMA
 */
-#include <OneWire.h> //library for the Ds18b20 temperature sensor
-#include <DallasTemperature.h> //library for the Ds18b20 temperature sensor
-#include <Servo.h> //library for the servo motor
-#include <Wire.h>
+#define trig 19
+#define echo 21
+#define relay1 22 // Relay or LED connected to GPIO 22
+#define relay2 23 // Relay or LED connected to GPIO 23
 
-// Data wire is plugged into digital pin 4 on the Arduino for temp sensor
-#define ONE_WIRE_BUS 4
+#include <OneWire.h> // Library for the DS18B20 temperature sensor
+#include <DallasTemperature.h> // Library for the DS18B20 temperature sensor
+#include <ESP32Servo.h> // Library for the servo motor
 
-// Setup a oneWire instance to communicate with any OneWire device
-OneWire oneWire(ONE_WIRE_BUS);	
+// Data wire is plugged into digital pin 18 for the temperature sensor
+#define ONE_WIRE_BUS 18
+
+// Setup a OneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);
 
 // Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
 
+// Create servo objects for the robotic arm
+Servo base_servo;
+Servo right_servo;
+Servo left_servo;
 
-Servo base_servo;  // create servo object to control a servo motor at the base of the arm
-Servo right_servo; // create servo object for the right servo motor
-Servo left_servo; // create servo object for the left servo motor
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
-int pH_Value;
-float Voltage;
-
-float calibration_value = 21.34; // for the ph sensor
-int phval = 0; 
-unsigned long int avgval; 
-int buffer_arr[10],temp;  // for the ph sensor
+// Variables for pH sensor
+float calibration_value = 21.34; // Calibration value for the pH sensor
+int buf[10]; // Buffer for pH sensor readings
 
 void setup() {
-  base_servo.attach(9);  // attaches the servo on pin 9 to the servo object
-  right_servo.attach(10);
-  left_servo.attach(11);
-  pinMode(pH_Value, INPUT);
-  sensors.begin();	// Start up the library
-  Serial.begin(9600);
+  base_servo.attach(15);  // Base servo connected to GPIO 15
+  right_servo.attach(2);  // Right servo connected to GPIO 2
+  left_servo.attach(4);   // Left servo connected to GPIO 4
+
+  pinMode(trig, OUTPUT);   // Ultrasonic trigger pin
+  pinMode(echo, INPUT);    // Ultrasonic echo pin
+  pinMode(relay1, OUTPUT); // Relay 1 control pin
+  pinMode(relay2, OUTPUT); // Relay 2 control pin
+
+  sensors.begin();	// Start the temperature sensor library
+  Serial.begin(9600); // Initialize Serial for debugging
 }
 
-void loop()
-{
-  delay(2000); // wait
-  for (pos = 0; pos <= 110; pos += 1) { // goes from 0 degrees to 110 degrees
-    // in steps of 1 degree
-   base_servo.write(pos);              // tell servo to go to position in variable 'pos' ie 0 degrees to 110 degrees
-    delay(23);                       // waits 23 ms for the servo to reach the position also determines how fast it goesn 
-  }
-  //delay(1000);
- 
-for (pos = 60; pos <= 135; pos += 1) { // goes from 60 degrees to 100 degrees, for the right servo motor
-    // in steps of 1 degree
-   right_servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(23);
-  }
- for (pos = 170; pos >= 30; pos -= 1) {  // activates the left servo motor
-   left_servo.write(pos);              
-    delay(23);
-  }
-for (pos = 135; pos <= 150; pos += 1) { // goes from 60 degrees to 100 degrees, for the right servo motor
-    // in steps of 1 degree
-   right_servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(23);
-  }
-for(int i=0;i<10;i++){ 
- buffer_arr[i]=analogRead(A0);
- delay(30);
- }
- for(int i=0;i<9;i++){
- for(int j=i+1;j<10;j++){
- if(buffer_arr[i]>buffer_arr[j]){
- temp=buffer_arr[i];
- buffer_arr[i]=buffer_arr[j];
- buffer_arr[j]=temp;
- }
- }
- }
- avgval=0;
- for(int i=2;i<10;i++)
- avgval+=buffer_arr[i];
- float volt=(float)avgval*5.0/1024/8; 
-float ph_act = -5.70 * volt + calibration_value;
-//Serial.println("PH is ");
-Serial.println(ph_act);
-//Serial.println(volt);
+int measure_distance() {
+  // Send trigger pulse
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
 
-//temprature sensor code in here.
+  // Read echo pulse duration
+  long duration = pulseIn(echo, HIGH);
 
-// Send the command to get temperatures
- sensors.requestTemperatures(); 
+  // Calculate distance (in cm)
+  int distance = (duration / 2) / 29.1;
+  return distance; // Return the measured distance
+}
 
- 
- delay(5000); // adding delay to allow the PH senosr record the PH of the water
+float measure_ph() {
+  // Collect multiple pH sensor readings
+  float avgValue = 0;
+  for (int i = 0; i < 10; i++) {
+    buf[i] = analogRead(33); // pH sensor connected to GPIO 33
+    delay(10);
+  }
+  for (int i = 0; i < 10; i++) {
+    avgValue += buf[i];
+  }
+  float pHvol = avgValue * 3.3 / 4095 / 10; // Convert ADC value to voltage
+  float phValue = -5.70 * pHvol + calibration_value; // Calculate pH value
+  return phValue;
+}
 
-  for (pos = 30; pos <= 170; pos += 1) { // goes from 110 degrees to 180 degrees
-    // in steps of 1 degree
-   left_servo.write(pos);              // tell servo to go to position in variable 'pos'
+void loop() {
+  delay(2000); // Wait before the loop starts
+
+  // Move base servo from 0 to 110 degrees
+  for (int pos = 0; pos <= 110; pos += 1) {
+    base_servo.write(pos);
     delay(23);
   }
 
- // delay(1000);
-
-  for (pos = 150; pos >= 60; pos -= 1) { 
-   right_servo.write(pos);              
-    delay(23);   
- }
-//delay(2000);
- for (pos = 110; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    base_servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(23);                       // waits 15 ms for the servo to reach the position
+  // Move right servo from 60 to 135 degrees
+  for (int pos = 60; pos <= 135; pos += 1) {
+    right_servo.write(pos);
+    delay(23);
   }
 
-//ph code
- /* pH_Value = analogRead(A0); //to calibrate the sensor
-  Voltage = pH_Value * (5.0 / 1023.0); 
-  Serial.println(Voltage); 
-  delay(500);*/
+  // Move left servo from 170 to 30 degrees
+  for (int pos = 170; pos >= 30; pos -= 1) {
+    left_servo.write(pos);
+    delay(23);
+  }
 
+  // Dip the sensor to measure pH
+  for (int pos = 135; pos <= 150; pos += 1) { 
+    right_servo.write(pos); // Move right servo to dipping position
+    delay(23);              
+  }
+  
+  // Measure pH while dipped
+  float phValue = measure_ph();
+  // Serial.print("pH Value: ");
+  // Serial.println(phValue);
 
+  // Measure temperature
+  sensors.requestTemperatures();
+  float temperature = sensors.getTempCByIndex(0);
+  // Serial.print("Temperature: ");
+  // Serial.println(temperature);
 
-Serial.print("PH is ");
-Serial.println(ph_act);
+  // Measure distance
+  int distance = measure_distance();
+  // Serial.print("Distance: ");
+  // Serial.println(distance);
 
-Serial.print("Temperature: ");
-Serial.print(sensors.getTempCByIndex(0));
-//Serial.print((char)176);//shows degrees character
-Serial.println("C  |  "); }
+  delay(3000);
 
+  Serial.print("pH Value: ");
+  Serial.println(phValue);
 
- 
- 
- 
-
-
-
-
-
-
-
-/*//temprature sensor code in here.
-
-// Send the command to get temperatures
- sensors.requestTemperatures(); 
-
-  //print the temperature in Celsius
   Serial.print("Temperature: ");
-  Serial.print(sensors.getTempCByIndex(0));
-  //Serial.print((char)176);//shows degrees character
-  Serial.println("C  |  "); }*/
+  Serial.println(temperature);
 
-  //yesterday 04th we added the ph sensor code inbtw the robotic arm code 
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  // Relay control logic
+  if (temperature <= 20.0 || temperature >= 33.0 || phValue <= 6.5 || phValue >= 9.0) {
+    digitalWrite(relay1, HIGH); // Turn ON relay1 if temperature and pH are in range
+    if (distance >= 14) {
+      digitalWrite(relay1, LOW); // Turn OFF relay1 if distance >= 15
+      digitalWrite(relay2, HIGH); // Turn ON relay2
+    } else if (distance <= 3) {
+      digitalWrite(relay2, LOW); // Turn OFF relay2 if distance <= 3
+    }
+  } else {
+    // Turn OFF both relays if conditions are not met
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, LOW);
+  }
+
+  // Return arm to its initial position
+
+  for (int pos = 30; pos <= 170; pos++) {
+    left_servo.write(pos);
+    delay(23);
+  }
+    for (int pos = 150; pos >= 135; pos -= 1) { //code swapped at 18/1124 23:16
+    right_servo.write(pos);
+    delay(23);
+  }
+  for (int pos = 135; pos >= 60; pos--) {
+    right_servo.write(pos);
+    delay(23);
+  }
+  for (int pos = 110; pos >= 0; pos--) {
+    base_servo.write(pos);
+    delay(23);
+  }
+}
